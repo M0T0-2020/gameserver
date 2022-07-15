7f33bef4f3b66e7941a69a04736b956fac8d1b18
M0T0 <57829067+M0T0-2020@users.noreply.github.com>
2022-07-15 03:06:07 +0000

    list room で無駄にstatusを取ってきている部分を修正

--- app/model.py
+++ app/model.py
@@ -133,7 +133,7 @@ def _get_room_member_cnt_rom_room_by_live_id(
         result = conn.execute(
             text(
                 f"""
-                SELECT room.room_id, status, live_id,  COUNT(member_id) AS joined_user_count
+                SELECT room.room_id, live_id, COUNT(member_id) AS joined_user_count
                 FROM member
                 INNER JOIN room ON member.room_id=room.room_id AND room.status=:waiting
                 {where_query} GROUP BY room.room_id
